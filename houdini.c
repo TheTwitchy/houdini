10150d93352e4d1c24b4fe6234d5ea4cc9f56a05
TheTwitchy <the.twitchy@gmail.com>
2014-12-30 14:10:46 -0600

    Version 1.1. Changed 'win' type to 'php', and added 'html' type.

--- houdini.c
+++ houdini.c
@@ -2,21 +2,22 @@
 #include <unistd.h>
 
 #define MAJ_VERSION 1
-#define MIN_VERSION 0
+#define MIN_VERSION 1
 #define AUTHOR "TheTwitchy"
 
 #define BUFFER_SIZE 1024
 
 /*Escape type options*/
 #define URL_ESC 0
-#define WIN_ESC 1
+#define PHP_ESC 1
+#define HTML_ESC 2
 
 void printHelp(char * name){
   printf("%s v%d.%d by %s\n", name, MAJ_VERSION, MIN_VERSION, AUTHOR);
   printf("\tinfo: a tool to escape stdin(default) or files into another ASCII readable form\n");
   printf("\tusage: %s [-e escape_type] [-i input_file] [-o output_file]\n", name);
   printf("\toptions:\n\t\t-i input filename\n\t\t-o output filename\n");
-  printf("\t\t-e escape type (url,win)\n");
+  printf("\t\t-e escape type (html,php,url)\n");
 }
 
 int main(int argc, char ** argv){
@@ -34,7 +35,8 @@ int main(int argc, char ** argv){
       return 0;
     case 'e':
       if(strcmp("url", optarg) == 0) escType = URL_ESC;
-      else if(strcmp("win", optarg) == 0) escType = WIN_ESC;
+      else if(strcmp("php", optarg) == 0) escType = PHP_ESC;
+      else if(strcmp("html", optarg) == 0) escType = HTML_ESC;
       else{
 	fprintf(stderr, "%s: unknown escape type '%s'\n", argv[0], optarg);
 	return -1;
@@ -75,8 +77,11 @@ int main(int argc, char ** argv){
       case URL_ESC:
 	fprintf(outFilePtr, "%%%02x", buf[n]);
 	break;
-      case WIN_ESC:
+      case PHP_ESC:
 	fprintf(outFilePtr, "\\x%02x", buf[n]);
+	break;
+      case HTML_ESC:
+	fprintf(outFilePtr, "&#%d;", buf[n]);
 	break;
       }
     }

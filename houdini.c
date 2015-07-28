963c667c17ffdf8cf85886176cca44beb9ae4dee
TheTwitchy <TheTwitchy@users.noreply.github.com>
2015-07-28 16:00:45 -0700

    Added urlalphanum encoding type.

--- houdini.c
+++ houdini.c
@@ -11,13 +11,14 @@
 #define URL_ESC 0
 #define PHP_ESC 1
 #define HTML_ESC 2
+#define URLALPHANUM_ESC 3
 
 void printHelp(char * name){
   printf("%s v%d.%d by %s\n", name, MAJ_VERSION, MIN_VERSION, AUTHOR);
   printf("\tinfo: a tool to escape stdin(default) or files into another ASCII readable form\n");
   printf("\tusage: %s [-e escape_type] [-i input_file] [-o output_file]\n", name);
   printf("\toptions:\n\t\t-i input filename\n\t\t-o output filename\n");
-  printf("\t\t-e escape type (html,php,url)\n");
+  printf("\t\t-e escape type (html,php,url,urlalphanum)\n");
 }
 
 int main(int argc, char ** argv){
@@ -37,6 +38,7 @@ int main(int argc, char ** argv){
       if(strcmp("url", optarg) == 0) escType = URL_ESC;
       else if(strcmp("php", optarg) == 0) escType = PHP_ESC;
       else if(strcmp("html", optarg) == 0) escType = HTML_ESC;
+      else if(strcmp("urlalphanum", optarg) == 0) escType = URLALPHANUM_ESC;
       else{
 	fprintf(stderr, "%s: unknown escape type '%s'\n", argv[0], optarg);
 	return -1;
@@ -76,6 +78,19 @@ int main(int argc, char ** argv){
       switch(escType){
       case URL_ESC:
 	fprintf(outFilePtr, "%%%02x", buf[n]);
+	break;
+      case URLALPHANUM_ESC:
+        /* Check to see if it's a number or character */
+	if ((buf[n] >= 0x30 && buf[n] <= 0x39) || (buf[n] >= 0x41 && buf[n] <= 0x5A) || (buf[n] >= 0x61 && buf[n] <= 0x7A)){
+          fprintf(outFilePtr, "%c", buf[n]);
+        }
+        /* Account for spaces. Could probably do this above too. */
+        else if (buf[n] == 0x20){
+          fprintf(outFilePtr, "+");
+        }
+        else{
+	  fprintf(outFilePtr, "%%%02x", buf[n]);
+        }
 	break;
       case PHP_ESC:
 	fprintf(outFilePtr, "\\x%02x", buf[n]);
